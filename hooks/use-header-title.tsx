7a123504505ab906353a3c8cdc407757f0464965
Steven Musumeche <steven@musumeche.com>
2020-05-06 18:05:01 -0500

    fix header alignment on android

--- hooks/use-header-title.tsx
+++ hooks/use-header-title.tsx
@@ -2,7 +2,7 @@ import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useContext, useLayoutEffect } from 'react';
 import { AppContext } from '../context/AppContext';
-import { useWindowDimensions } from 'react-native';
+import { useWindowDimensions, Platform } from 'react-native';
 
 export const useHeaderTitle = (title: string) => {
   const { width } = useWindowDimensions();
@@ -12,7 +12,10 @@ export const useHeaderTitle = (title: string) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: title + ' for ' + activeLocation.name,
-      headerTitleStyle: { width: width - 70, textAlign: 'center' },
+      headerTitleStyle: {
+        width: width - 70,
+        textAlign: Platform.OS === 'ios' ? 'center' : 'left',
+      },
     });
   }, [activeLocation, navigation, title, width]);
 };
