e6ddde57df12d573aac2e108a429fde743ac03ce
Steven Musumeche <steven@musumeche.com>
2020-11-02 16:58:26 -0600

    wip rotation

--- components/FullScreenGraph.tsx
+++ components/FullScreenGraph.tsx
@@ -1,8 +1,12 @@
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, useWindowDimensions } from 'react-native';
+import {
+  SafeAreaView,
+  useSafeArea,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 import { VictoryScatter } from 'victory-native';
 import { white } from '../colors';
 import { NowStackParams } from '../screens/NowScreen';
@@ -17,6 +21,10 @@ interface Props {
 }
 
 const FullScreenGraph: React.FC<Props> = ({ route, navigation }) => {
+  const safeArea = useSafeArea();
+  const window = useWindowDimensions();
+  console.log(safeArea);
+
   // async function changeScreenOrientation() {
   //   await lockAsync(OrientationLock.LANDSCAPE_LEFT);
   // }
@@ -40,11 +48,18 @@ const FullScreenGraph: React.FC<Props> = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: white,
     justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: white,
   },
   graphWrapper: {
-    paddingHorizontal: 10,
+    // paddingHorizontal: 10,
+    // paddingVertical: 50,
+    //transform: [{ rotate: '90deg' }],
+    flex: 1,
+    flexGrow: 1,
+    // justifyContent: 'center',
+    // alignItems: 'flex-start',
   },
 });
 
--- components/Graph.tsx
+++ components/Graph.tsx
@@ -27,7 +27,7 @@ const Graph: React.FC<Props> = ({
   fullScreen = false,
   onPress,
 }) => {
-  const windowWidth = useWindowDimensions().width;
+  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
 
   // reduce the number of data points to display on the graph
   const mod = Math.ceil(data.length / 72);
@@ -35,75 +35,94 @@ const Graph: React.FC<Props> = ({
     data = data.filter((_: any, i: number) => i % mod === 0);
   }
 
-  const width = fullScreen ? windowWidth - 20 : windowWidth / 2 - 50;
-  const height = fullScreen ? 300 : 140;
+  const width = fullScreen ? windowHeight - 100 : windowWidth / 2 - 50;
+  const height = fullScreen ? windowWidth - 30 : 140;
   const tickCount = fullScreen ? 8 : 2;
 
   return (
     <View style={styles.container}>
-      {wrapWithTouchable(
-        <VictoryChart
-          padding={{
-            left: 23,
-            top: 10,
-            right: 10,
-            bottom: fullScreen ? 24 : 12,
-          }}
-          domainPadding={{ y: [30, 30] }}
-          height={height}
-          width={width}
-        >
-          <VictoryAxis
-            fixLabelOverlap={false}
-            tickCount={tickCount}
-            tickFormat={(date: string) => {
-              const dateObj = new Date(date);
-              if (fullScreen) {
-                return format(dateObj, 'ccc') + '\n' + format(dateObj, 'ha');
+      <View
+        style={
+          fullScreen
+            ? {
+                transform: [
+                  { rotate: '90deg' },
+                  { translateX: width / 2 - 180 },
+                  { translateY: height / 2 },
+                ],
               }
+            : {}
+        }
+      >
+        {wrapWithTouchable(
+          <VictoryChart
+            padding={{
+              left: 23,
+              top: 10,
+              right: 10,
+              bottom: fullScreen ? 24 : 12,
+            }}
+            domainPadding={{ y: [30, 30] }}
+            height={height}
+            width={width}
+          >
+            <VictoryAxis
+              fixLabelOverlap={false}
+              tickCount={tickCount}
+              tickFormat={(date: string) => {
+                const dateObj = new Date(date);
+                if (fullScreen) {
+                  return format(dateObj, 'ccc') + '\n' + format(dateObj, 'ha');
+                }
 
-              const hourDiff = Math.abs(differenceInHours(dateObj, new Date()));
-              if (hourDiff >= 46) {
-                return '-2d';
-              } else if (hourDiff >= 22) {
-                return '-1d';
-              }
-              return 'now';
-            }}
-            style={{
-              tickLabels: { fontSize: 11, padding: 1 },
-              grid: {
-                stroke: gray[500],
-                strokeDasharray: '6, 6',
-                strokeWidth: fullScreen ? 0.5 : 1,
-              },
-            }}
-          />
-          <VictoryAxis
-            scale={{ x: 'time' }}
-            dependentAxis
-            style={{ tickLabels: { fontSize: 11, padding: 3 } }}
-            tickFormat={(x, i, ticks) => {
-              // if the ticks with no decimals contain duplicates, then use a decimal
-              const withNoDecimals = ticks.map((tick) => tick.toFixed(0));
-              if (withNoDecimals.length > new Set([...withNoDecimals]).size) {
-                return x.toFixed(1);
-              }
-              return x.toFixed(0);
-            }}
-          />
-          <VictoryGroup data={data}>
-            <VictoryLine
-              interpolation="natural"
+                const hourDiff = Math.abs(
+                  differenceInHours(dateObj, new Date()),
+                );
+                if (hourDiff >= 46) {
+                  return '-2d';
+                } else if (hourDiff >= 22) {
+                  return '-1d';
+                }
+                return 'now';
+              }}
               style={{
-                data: { stroke: yellow[700], strokeWidth: fullScreen ? 2 : 1 },
+                tickLabels: { fontSize: 11, padding: 1 },
+                grid: {
+                  stroke: gray[500],
+                  strokeDasharray: '6, 6',
+                  strokeWidth: fullScreen ? 0.5 : 1,
+                },
               }}
             />
-            {children}
-          </VictoryGroup>
-        </VictoryChart>,
-        onPress,
-      )}
+            <VictoryAxis
+              scale={{ x: 'time' }}
+              dependentAxis
+              style={{ tickLabels: { fontSize: 11, padding: 3 } }}
+              tickFormat={(x, i, ticks) => {
+                // if the ticks with no decimals contain duplicates, then use a decimal
+                const withNoDecimals = ticks.map((tick) => tick.toFixed(0));
+                if (withNoDecimals.length > new Set([...withNoDecimals]).size) {
+                  return x.toFixed(1);
+                }
+                return x.toFixed(0);
+              }}
+            />
+            <VictoryGroup data={data}>
+              <VictoryLine
+                interpolation="natural"
+                style={{
+                  data: {
+                    stroke: yellow[700],
+                    strokeWidth: fullScreen ? 2 : 1,
+                  },
+                }}
+              />
+              {children}
+            </VictoryGroup>
+          </VictoryChart>,
+          onPress,
+        )}
+      </View>
     </View>
   );
 };
@@ -113,6 +132,7 @@ export default Graph;
 const styles = StyleSheet.create({
   container: {
     width: '100%',
+    height: '100%',
     overflow: 'hidden',
   },
 });
